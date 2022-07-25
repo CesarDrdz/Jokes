88157fe5dab52d4851edc284dd90fc6abbcbb36d
John Doe <johndoe@example.com>
2022-07-25 01:37:52 -0700

    v2 FULL CRUD

--- server/controllers/jokes.controller.js
+++ server/controllers/jokes.controller.js
@@ -24,6 +24,45 @@ module.exports.findAllJokes = (req, res) => {
 module.exports.createNewJoke = (req, res) => {
     // db.joke.insertOne({setup:" ", punchline: "  "})
     Joke.create(req.body)
-        .then(newlyCreatedJoke => res.json({ user: newlyCreatedJoke }))
+        .then(newlyCreatedJoke => res.json({ joke: newlyCreatedJoke }))
+        .catch(err => res.json({ message: 'Something went wrong', error: err }));
+}
+
+// find one 
+module.exports.findOneSingleJoke = (req, res) => {
+
+    // this extra var(id) at the end of params must match our route in order for it to work ("/api/jokes/:id")
+    Joke.findOne({ _id: req.params.id })
+        .then(oneSingleJoke => res.json({ joke: oneSingleJoke }))
+        .catch(err => res.json({ message: 'Something went wrong', error: err }));
+}
+
+
+// -------UPDATE from the plateform
+// module.exports.updateExistingJoke = (req, res) => {
+//     joke.findOneAndUpdate(
+//         { _id: req.params.id },
+//         req.body,
+//         { new: true, runValidators: true }
+//     )
+//         .then(updatedJoke => res.json({ joke: updatedJoke }))
+//         .catch(err => res.json({ message: 'Something went wrong', error: err }));
+// }
+
+
+// UPDATE WITH SENSAI JOHN
+module.exports.updateExistingJoke = (req, res) => {
+    console.log(req.params);
+    // destructuring 
+    const { id } = req.params
+    Joke.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true })
+        .then(updatedJoke = res.json({ updatedJoke }))
+        .catch(err => res.json({ message: 'Something Went Wrong', error: err }));
+}
+
+// DELETE
+module.exports.deleteAnExistingJoke = (req, res) => {
+    Joke.deleteOne({ _id: req.params.id })
+        .then(result => res.json({ result: result }))
         .catch(err => res.json({ message: 'Something went wrong', error: err }));
 }
--- server/routes/jokes.routes.js
+++ server/routes/jokes.routes.js
@@ -4,4 +4,8 @@ console.log(JokeController);
 
 module.exports = (app) => {
     app.get("/api/jokes", JokeController.findAllJokes)
+    app.post("/api/jokes", JokeController.createNewJoke)
+    app.get("/api/jokes/:id", JokeController.findOneSingleJoke)
+    app.put("/api/jokes/:id", JokeController.updateExistingJoke)
+    app.delete("/api/jokes/:id", JokeController.deleteAnExistingJoke)
 }
